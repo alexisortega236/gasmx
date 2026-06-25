ff6d3bdef0a887898920e723e7db99da74fa7918
alexisortega236 <gonzalezalexis236@gmail.com>
2026-06-25 12:32:54 -0600

    Create Laravel runtime cache directories on startup

--- docker/start.sh
+++ docker/start.sh
@@ -3,9 +3,18 @@ set -eu
 
 : "${PORT:=10000}"
 
+mkdir -p \
+  storage/framework/cache/data \
+  storage/framework/sessions \
+  storage/framework/views \
+  bootstrap/cache
+
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R ug+rwx storage bootstrap/cache
+
 sed -ri "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -ri "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" \
-    /etc/apache2/sites-available/000-default.conf
+  /etc/apache2/sites-available/000-default.conf
 
 echo "Running migrations..."
 php artisan migrate --force
